dfc89e6d61113f696997ab82485cfb0797186112
Abdon Morales <39339393+Server2356@users.noreply.github.com>
2021-01-26 13:35:43 -0600

    Research Unix Shell v1.0.0 (C) 2021 Morales Research Corporation

---
+++ patchlevel.h
@@ -0,0 +1,13 @@
+/*
+ * patchlevel.h: Our life story.
+ */
+#ifndef _h_patchlevel
+#define _h_patchlevel
+
+#define ORIGIN "Astron"
+#define REV 6
+#define VERS 22
+#define PATCHLEVEL 03
+#define DATE "2020-11-18"
+
+#endif /* _h_patchlevel */

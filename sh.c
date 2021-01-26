e2ffc5839ed332b195af7c8e7e6b46855fb4efa4
Abdon Morales <39339393+abdonmorales@users.noreply.github.com>
2021-01-26 13:41:15 -0600

    Update sh.c

--- sh.c
+++ sh.c
@@ -34,7 +34,7 @@
 
 #ifndef lint
 char    copyright[] =
-"@(#) Copyright (c) 1991 The Regents of the University of California.\n\
+"@(#) Copyright (c) 2021 Morales Research Corporation .\n\
  All rights reserved.\n";
 #endif /* not lint */
 
@@ -46,33 +46,10 @@ extern int MapsAreInited;
 extern int NLSMapsAreInited;
 
 /*
- * C Shell
+ * Research Unix Shell
  *
- * Bill Joy, UC Berkeley, California, USA
- * October 1978, May 1980
- *
- * Jim Kulp, IIASA, Laxenburg, Austria
- * April 1980
- *
- * Filename recognition added:
- * Ken Greer, Ind. Consultant, Palo Alto CA
- * October 1983.
- *
- * Karl Kleinpaste, Computer Consoles, Inc.
- * Added precmd, periodic/tperiod, prompt changes,
- * directory stack hack, and login watch.
- * Sometime March 1983 - Feb 1984.
- *
- * Added scheduled commands, including the "sched" command,
- * plus the call to sched_run near the precmd et al
- * routines.
- * Upgraded scheduled events for running events while
- * sitting idle at command input.
- *
- * Paul Placeway, Ohio State
- * added stuff for running with twenex/inputl  9 Oct 1984.
- *
- * ported to Apple Unix (TM) (OREO)  26 -- 29 Jun 1987
+ * Abdon Morales, Morales Research Corp, Austin, TX
+ * October 1978 (Bell Labs), January 2021
  */
 
 jmp_buf_t reslab IZERO_STRUCT;
